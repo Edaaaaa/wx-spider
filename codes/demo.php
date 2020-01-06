33298f7b666def6128d760537d3380f67e0382ae
Wssss <qq690380946@163.com>
2020-01-06 11:26:46 +0800

    Create demo.php

---
+++ codes/demo.php
@@ -0,0 +1,23 @@
+<?php
+
+$auth = "XXXXXX"; //联系作者获取
+$biz = "xxxxxx" //公众号biz, 如果不会获取,请参考get_biz.php代码
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://121.196.196.117/Home/info/get_newprofile?auth={$auth}&biz={$biz}",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 10,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
