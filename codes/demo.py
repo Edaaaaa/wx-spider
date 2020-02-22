c8b98cbaf0fb75bffb8da43d186944b36ae0aa77
Wssss <qq690380946@163.com>
2020-02-22 16:08:19 +0800

    Update demo.py

--- codes/demo.py
+++ codes/demo.py
@@ -1,10 +1,10 @@
 import requests
 host = 'xxxx'       #服务器ip
 auth = 'xxxx'       #auth
-art_url = 'xxxx'    #文章链接
-request_url = host+"/Home/info/get_read_num"
+biz  = 'xxxx'       #公众号biz
+request_url = host+"/Home/info/get_newprofile"
 
-payload = {'auth': auth,'url': art_url}
+payload = {'auth': auth,'biz': biz}
 headers = {}
 response = requests.request("POST", request_url, headers=headers, data = payload)
 print(response.text)
